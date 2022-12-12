564b95202028ce70ba555911fa66e8a1cc09e4a3
wendy barrios <wbarrios45@yahoo.com>
2022-12-12 13:38:34 -0800

    Added ingredients.html and added cover page

--- api/js/fastfood.js
+++ api/js/fastfood.js
@@ -5,7 +5,6 @@ document.querySelector('#fastFoodButton').addEventListener('click', fastFood)
 //Start of function to clear out data from input form 
 function clearField() {
   document.querySelector("#searchfastfood").value = ""
- 
 }
 //End of function to clear out data from input form 
 
@@ -52,37 +51,64 @@ function fastFood(){
 const createCard = (foodItem) => {
   document.querySelector("#foodInfo1").insertAdjacentHTML(
     "beforeend",
-    `<section class="container4 my-5" style="width: 470px; height: 470px;">
-     <div class="row g-3">
-      
-            <div class="col-lg-6">
-              <div class="p-d mt-4">
-                    <h3 class="card-title text-center">${foodItem.food.label}</h3>
-                    <p class="card-text">Brand: ${foodItem.food.brand}</p>
-                    <p class="card-text">Serving Size: ${foodItem.food.servingSizes[0].quantity} ${foodItem.food.servingSizes[0].label}</p>
-                    <p class="card-text">Calories (kcal): ${Math.round(foodItem.food.nutrients.ENERC_KCAL)}</p>
-                    <p class="card-text">Total Fat (g): ${Math.round(foodItem.food.nutrients.FAT)}</p>
-                    <p class="card-text">Total Carbohydrate (g): ${Math.round(foodItem.food.nutrients.CHOCDF)}</p>
-                    <p class="card-text">Total Fiber (g): ${Math.round(foodItem.food.nutrients.FIBTG)}</p>
-                    <p class="card-text">Total Protein (g): ${Math.round(foodItem.food.nutrients.PROCNT)}</p>
-                    
-                    <i id="heartBTN-${foodItem.food.label}" class="fas fa-heart hidden heart fa-lg p-1 my-1 mr-3" tabindex="0" aria-label="like content" title="I like it"><span class="likeSpan likeSpan${foodItem.food.label}">Like Me</span></i>
-                    </div>
-                    </div>
-                  </div>
-                </div>
-            </div>
-        
-    </div>
-   </section>
-   
-   
-   
-  </div>
+    `<section class="container4 my-5" style="width: 470px; height: 470px; float:left">
+    <div class="row g-3">
+           <div class="col-lg-6">
+             <div class="p-d mt-4">
+                   <h3 class="card-title text-center">${foodItem.food.label}</h3>
+                   <p class="card-text">Brand: ${foodItem.food.brand}</p>
+                   <p class="card-text">Serving Size: ${foodItem.food.servingSizes[0].quantity} ${foodItem.food.servingSizes[0].label}</p>
+                   <p class="card-text">Calories (kcal): ${Math.round(foodItem.food.nutrients.ENERC_KCAL)}</p>
+                   <p class="card-text">Total Fat (g): ${Math.round(foodItem.food.nutrients.FAT)}</p>
+                   <p class="card-text">Total Carbohydrate (g): ${Math.round(foodItem.food.nutrients.CHOCDF)}</p>
+                   <p class="card-text">Total Fiber (g): ${Math.round(foodItem.food.nutrients.FIBTG)}</p>
+                   <p class="card-text">Total Protein (g): ${Math.round(foodItem.food.nutrients.PROCNT)}</p>
+                   
+                   <i id="heartBTN-${foodItem.food.label}" class="fas fa-heart hidden heart fa-lg p-1 my-1 mr-3" tabindex="0" aria-label="like content" title="I like it"><span class="likeSpan likeSpan${foodItem.food.label}">Like Me</span></i>
+                   </div>
+                   </div>
+                 </div>
+               </div>
+           </div>
+       
+   </div>
+  </section>
+ </div>
 </div>
-</section>`
+</section> 
+`
   )
 // End of Food Card
 
   }
+
+
+
+
+
+
+
+//   <section id="gallery">
+//    <div class="container">
+//      <div class="row">
+//      <div class="col-lg-4 mb-4">
+//      <div class="card">
+//        <div class="card-body">
+//        <h3 class="card-title">${foodItem.food.label}</h3>
+//        <p class="card-text">Brand: ${foodItem.food.brand}</p>
+//        <p class="card-text">Serving Size: ${foodItem.food.servingSizes[0].quantity} ${foodItem.food.servingSizes[0].label}</p>
+//        <p class="card-text">Calories (kcal): ${Math.round(foodItem.food.nutrients.ENERC_KCAL)}</p>
+//        <p class="card-text">Total Fat (g): ${Math.round(foodItem.food.nutrients.FAT)}</p>
+//        <p class="card-text">Total Carbohydrate (g): ${Math.round(foodItem.food.nutrients.CHOCDF)}</p>
+//        <p class="card-text">Total Fiber (g): ${Math.round(foodItem.food.nutrients.FIBTG)}</p>
+//        <p class="card-text">Total Protein (g): ${Math.round(foodItem.food.nutrients.PROCNT)}</p>
+       
+//        <i id="heartBTN-${foodItem.food.label}" class="fas fa-heart hidden heart fa-lg p-1 my-1 mr-3" tabindex="0" aria-label="like content" title="I like it"><span class="likeSpan likeSpan${foodItem.food.label}">Like Me</span></i>
+//        </div>
+//       </div>
+     
+    
+//    </div>
+//  </div>
+//  </section>
 
